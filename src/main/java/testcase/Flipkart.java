b38a6c576c1dd00237d1a1bddb07fadd1ce459b2
madanraj thangaraj <madan231193@gmail.com>
2020-04-17 00:21:41 +0530

    FB-DRIVER-UPDATE | Madan

--- src/main/java/testcase/Flipkart.java
+++ src/main/java/testcase/Flipkart.java
@@ -14,7 +14,7 @@ public class Flipkart {
 @Test
 	public void execute() {
 
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
