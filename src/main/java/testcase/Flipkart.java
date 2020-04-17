e9e3e99e22cdc7e3bf94c97f270010e4f8049c4d
madanraj thangaraj <madan231193@gmail.com>
2020-04-17 15:21:04 +0530

    FB-IE-Driver-update | Madan

--- src/main/java/testcase/Flipkart.java
+++ src/main/java/testcase/Flipkart.java
@@ -5,22 +5,23 @@ import java.util.List;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-
 public class Flipkart {
+	public RemoteWebDriver driver;
 
-@Test
-	public void execute() throws InterruptedException {
-	Thread.sleep(3000);
-	System.out.println("i am inside test");
-	
-	
+	@Test
+	public void execute() {
 
-		
-		}
+		System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.close();
 
 	}
 
-
+}
